ec6f03939f6fe340b37bc02f48b5ae25662cd1e3
crazyAT8 <88wwatson@gmail.com>
2024-07-30 21:05:11 -0500

    init 7/30/24

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  // Properties
+
+  const [walletAddress, setWalletAddress] = useState("");
+
+  // Helper Function
+
+  // Requests access to the user's METAMASK Wallet
+  // https://metamask.io
+  async function requestAccount() {
+    console.log('Requesting account...')
+
+    // Check if MetaMask Extension exists
+    if(window.ethereum) {
+      console.log('detected')
+
+      try {
+        const accounts = await window.ethereum.request({ 
+          method: "eth_requestAccounts",
+        });
+        // console.log(accounts);
+        setWalletAddress(accounts[0]);
+      } catch (error) {
+        console.log('Error connecting...');
+      }
+
+    } else {
+      console.log('MetaMask not detected');
+    }
+  }
+
+  // async function connectWallet() {
+  //   if(typeof window.ethereum !== 'undefined') {
+  //     await requestAccount();
+
+  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  //   }
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button
+        
+        onClick={requestAccount}
+        
+        >Connect Wallet</button>
+        <h3>Wallet Address: {walletAddress}</h3>
       </header>
     </div>
   );
